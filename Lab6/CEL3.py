de49de017b301fe34378270cdf99cacaf906ff22
Pangpondx <156653875+Pangpondx@users.noreply.github.com>
2024-01-15 00:55:01 -0800

    Update CEL3.py

--- Lab6/CEL3.py
+++ Lab6/CEL3.py
@@ -1,1 +1,21 @@
-
+import RPi.GPIO as GPIO
+LED = 18
+SW = 22
+count = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SW, GPIO.IN)
+GPIO.setup(LED, GPIO.OUT)
+try :
+    while True :
+        if GPIO.wait_for_edge (SW, GPIO.RISING):
+            if count == 0:
+              GPIO.output (LED, True)       
+              print(f"LED => ON ")
+              count = count + 1
+            elif count == 1:
+              GPIO.output (LED, False)         
+              print(f"LED => OFF ")    
+              count = count - 1
+except KeyboardInterrupt:
+    GPIO.cleanup()
+print("\nBye...")   
