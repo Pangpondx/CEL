876ec3ffe3c8bcb1f11b2f08b64853f42be68264
Pangpondx <156653875+Pangpondx@users.noreply.github.com>
2024-01-15 01:30:42 -0800

    Update CEL4.py

--- Lab6/CEL4.py
+++ Lab6/CEL4.py
@@ -1,1 +1,57 @@
-
+import RPi.GPIO as GPIO
+LED1 = 14
+LED2 = 15
+LED3 = 18
+SW = 22
+count = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SW, GPIO.IN)
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+GPIO.setup(LED3, GPIO.OUT)
+try :
+    while True :
+        if GPIO.wait_for_edge (SW, GPIO.RISING):
+            if count == 0:
+              GPIO.output (LED1, False)       
+              GPIO.output (LED2, False)  
+              GPIO.output (LED3, False)  
+              count = count + 1
+            elif count == 1:
+              GPIO.output (LED1, False)       
+              GPIO.output (LED2, False)  
+              GPIO.output (LED3, True)                       
+              count = count + 1
+            elif count == 2:
+              GPIO.output (LED1, False)       
+              GPIO.output (LED2, True)  
+              GPIO.output (LED3, False)  
+              count = count + 1
+            elif count == 3:
+              GPIO.output (LED1, False)       
+              GPIO.output (LED2, True)  
+              GPIO.output (LED3, True)  
+              count = count + 1
+            elif count == 4:
+              GPIO.output (LED1, True)       
+              GPIO.output (LED2, False)  
+              GPIO.output (LED3, False)  
+              count = count + 1
+            elif count == 5:
+              GPIO.output (LED1, True)       
+              GPIO.output (LED2, False)  
+              GPIO.output (LED3, True)               
+              count = count + 1
+            elif count == 6:
+              GPIO.output (LED1, True)       
+              GPIO.output (LED2, True)  
+              GPIO.output (LED3, False)  
+              count = count + 1
+            elif count == 7:
+              GPIO.output (LED1, True)       
+              GPIO.output (LED2, True)  
+              GPIO.output (LED3, True)   
+              count = count - 7
+except KeyboardInterrupt:
+    GPIO.cleanup()
+print("\nBye...")   
